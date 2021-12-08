1fd9d28c6892a96707a00d7c17d6ed1d059d7e77
SungeunAmberKim <sungeunkim@cpp.edu>
2021-12-08 19:43:39 +0900

    turn off canonical mode

--- kilo.c
+++ kilo.c
@@ -2,16 +2,15 @@
 #include <termios.h>
 #include <unistd.h>
 struct termios orig_termios;
-void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
+void disableRawMode() { … }
 void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO);
+  raw.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 int main() {
   enableRawMode();
   char c;
