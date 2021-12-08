e591c30be465d30d4ec40c4deec3b0cc06ea3a1e
SungeunAmberKim <sungeunkim@cpp.edu>
2021-12-08 17:28:18 +0900

    add lsh_execute

--- source.c
+++ source.c
@@ -84,6 +84,23 @@ int lsh_launch(char **args)
     return 1;
 }
 
+int lsh_execute(char **args)
+{
+    int i;
+
+    if(args[0] == NULL){
+        // An empty command was entered
+        return 1;
+    }
+
+    for (i = 0; i < lsh_num_builtins(); i++){
+        if(strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+    return lsh_launch(args);
+}
+
 
 #define LSH_RL_BUFSIZE 1024
 char *lsh_read_line(void)
