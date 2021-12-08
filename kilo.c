c21a266a259445a7e4455b7fe9531640d181769d
SungeunAmberKim <sungeunkim@cpp.edu>
2021-12-09 00:41:35 +0900

    add render and rsize

--- kilo.c
+++ kilo.c
@@ -30,7 +30,9 @@ enum editorKey {
 /*** data ***/
 typedef struct erow {
   int size;
+  int rsize;
   char *chars;
+  char *render;
 } erow;
 struct editorConfig {
   int cx, cy;
@@ -149,6 +151,8 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
   E.numrows++;
 }
 /*** file i/o ***/
