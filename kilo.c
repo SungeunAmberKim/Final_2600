8ad84f9302be9ce93078c6ead72be754d2519781
SungeunAmberKim <sungeunkim@cpp.edu>
2021-12-09 15:14:55 +0900

    add backspace

--- kilo.c
+++ kilo.c
@@ -20,6 +20,7 @@
 #define KILO_TAB_STOP 8
 #define CTRL_KEY(k) ((k) & 0x1f)
 enum editorKey {
+  BACKSPACE = 127,
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
@@ -286,6 +287,9 @@ void editorMoveCursor(int key) {
 void editorProcessKeypress() {
   int c = editorReadKey();
   switch (c) {
+    case '\r':
+      /* TODO */
+      break;
     case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
@@ -297,6 +301,11 @@ void editorProcessKeypress() {
     case END_KEY:
       if (E.cy < E.numrows)
         E.cx = E.row[E.cy].size;
+      break;
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL_KEY:
+      /* TODO */
       break;
     case PAGE_UP:
     case PAGE_DOWN:
@@ -317,6 +326,9 @@ void editorProcessKeypress() {
     case ARROW_LEFT:
     case ARROW_RIGHT:
       editorMoveCursor(c);
+      break;
+    case CTRL_KEY('l'):
+    case '\x1b':
       break;
     default:
       editorInsertChar(c);
