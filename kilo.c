9cfcf2b93661d3c7c4595c6aed657259a88a7eff
SungeunAmberKim <sungeunkim@cpp.edu>
2021-12-09 15:24:57 +0900

    add editorDelChar

--- kilo.c
+++ kilo.c
@@ -223,6 +223,14 @@ void editorInsertChar(int c) {
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
 }
+void editorDelChar() {
+  if (E.cy == E.numrows) return;
+  erow *row = &E.row[E.cy];
+  if (E.cx > 0) {
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+  }
+}
 /*** file i/o ***/
 char *editorRowsToString(int *buflen) {
   int totlen = 0;
