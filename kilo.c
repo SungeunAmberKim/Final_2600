7550a49e46ad1a332b153f3f772e56b656ff7681
SungeunAmberKim <sungeunkim@cpp.edu>
2021-12-09 15:16:21 +0900

    add editorSave()

--- kilo.c
+++ kilo.c
@@ -5,6 +5,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -242,6 +243,16 @@ void editorOpen(char *filename) {
   }
   free(line);
   fclose(fp);
+}
+void editorSave() {
+  if (E.filename == NULL) return;
+  int len;
+  char *buf = editorRowsToString(&len);
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  ftruncate(fd, len);
+  write(fd, buf, len);
+  close(fd);
+  free(buf);
 }
 
 /*** append buffer ***/
